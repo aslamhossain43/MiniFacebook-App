932da695b8944b14f1bbd7f42f968c0fef87f477
Atif Aslam <aslam.renu.soft43@gmail.com>
2019-02-08 19:55:03 -0800

    update completed

--- MiniFacebook-App/src/app/about/about.component.ts
+++ MiniFacebook-App/src/app/about/about.component.ts
@@ -5,6 +5,7 @@ import { AboutService } from './about.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Response } from '@angular/http';
 import { ProfessionalSkill } from './about.professionalskill';
+import { College } from './about.college';
 
 @Component({
   selector: 'app-about',
@@ -13,14 +14,18 @@ import { ProfessionalSkill } from './about.professionalskill';
 })
 export class AboutComponent implements OnInit {
   uid: string;
-   // FOR NGX BOOTSTRAP  MODAL
+   // --------------------------------------------------------------------------------
    public modalRef: BsModalRef;
-   // WORKPLACE OBJECTS ARE HERE
+   // --------------------------------------------------------------------------------
 workplace = new Workplace();
 workplaces: Workplace[];
-// PROFESSIONAL SKILLS OBJECTS HERE
+// ------------------------------------------------------------------------------------
 professionalSkill = new ProfessionalSkill();
 professionalSkills: ProfessionalSkill[];
+// ------------------------------------------------------------------------------------
+college = new College();
+colleges: College[];
+// ------------------------------------------------------------------------------------
   constructor( /* FOR NGX BOOTSTRAP  MODAL*/
     private modalService: BsModalService, private aboutService: AboutService,
     private af: AngularFireAuth) { 
@@ -28,45 +33,53 @@ professionalSkills: ProfessionalSkill[];
         this.uid = auth.uid;
         this.getWorkplaceByUID();
         this.getProfessionalSkillsByUID();
+        this.getCollegeByUID();
       });
     }
+    // ----------------------------------------------------------------------------------
  // FOR NGX BOOTSTRAP  MODAL
  public openModal(template: TemplateRef<any>) {
    this.modalRef = this.modalService.show(template);
 }
+// --------------------------------------------------------------------------------------
   ngOnInit(): void {
     this.getWorkplaceByUID();
    this.getProfessionalSkillsByUID();
+   this.getCollegeByUID();
   }
 
-
+// ---------------------------------------------------------------------------------------
 // WORKPLACE ALL METHODS ARE HERE
 saveWorkplace(): void {
 this.workplace.uid = this.uid;
   this.aboutService.saveWorkplace(this.workplace)
   .subscribe(response => {
 this.getWorkplaceByUID();
-
+this.getProfessionalSkillsByUID();
+this.getCollegeByUID();
 }, (error) => {
 
 });
 }
+// ----------------------
 workplaceRefresh() {
   this.workplace.workPlace = null;
 }
-
+// ----------------------
 getWorkplaceByUID(): void {
 this.aboutService.getWorkplaceByUID(this.uid)
 .subscribe((workplace) => {
   this.workplaces = workplace;
 });
 }
+// -----------------------------
 getWorkplaceById(id: string): void {
   this.aboutService.getWorkplaceById(id)
   .subscribe((workplace) => {
     this.workplace = workplace;
   });
 }
+// -----------------------------------
 deleteWorkplaceById(id: string): void {
   this.aboutService.deleteWorkplaceById(id)
   .subscribe((response: Response) => {
@@ -79,7 +92,7 @@ deleteWorkplaceById(id: string): void {
 
 
 
-
+// -----------------------------------------------------------------------------------------
 
 // PROFESSIONAL SKILLS ALL METHODS ARE HERE
 saveProfessionalSkills(): void {
@@ -88,27 +101,31 @@ saveProfessionalSkills(): void {
     .subscribe(response => {
   this.getWorkplaceByUID();
   this.getProfessionalSkillsByUID();
+this.getCollegeByUID();
   
   }, (error) => {
   
   });
   }
+  // -----------------------------
   professionalSkillsRefresh() {
     this.professionalSkill.professionalSkill = null;
   }
-  
+// -------------------------------
   getProfessionalSkillsByUID(): void {
   this.aboutService.getProfessionalSkillsByUID(this.uid)
   .subscribe((professionalSkills) => {
     this.professionalSkills = professionalSkills;
   });
   }
+  // -------------------------------
   getProfessionalSkillsById(id: string): void {
     this.aboutService.getProfessionalSkillsById(id)
     .subscribe((professionalSkills) => {
       this.professionalSkill = professionalSkills;
     });
   }
+  // --------------------------------
   deleteProfessionalSkillsById(id: string): void {
     this.aboutService.deleteProfessionalSkillsById(id)
     .subscribe((response: Response) => {
@@ -117,6 +134,48 @@ saveProfessionalSkills(): void {
   
     });
   }
+  // -----------------------------------------------------------------------------------------
+
+// CPLLEGE ALL METHODS ARE HERE
+saveCollege(): void {
+  this.college.uid = this.uid;
+    this.aboutService.saveCollege(this.college)
+    .subscribe(response => {
+  this.getWorkplaceByUID();
+  this.getProfessionalSkillsByUID();
+  this.getCollegeByUID();
+  }, (error) => {
+  
+  });
+  }
+  // -----------------------------
+  CollegeRefresh() {
+    this.college.college = null;
+  }
+// -------------------------------
+  getCollegeByUID(): void {
+  this.aboutService.getCollegeByUID(this.uid)
+  .subscribe((college) => {
+    this.colleges = college;
+  });
+  }
+  // -------------------------------
+  getCollegeById(id: string): void {
+    this.aboutService.getCollegeById(id)
+    .subscribe((college) => {
+      this.college = college;
+    });
+  }
+  // --------------------------------
+  deleteCollegeById(id: string): void {
+    this.aboutService.deleteCollegeById(id)
+    .subscribe((response: Response) => {
+   this.getCollegeByUID();
+    }, (error) => {
+  
+    });
+  }
+
 }
 
 
--- MiniFacebook-App/src/app/about/about.service.ts
+++ MiniFacebook-App/src/app/about/about.service.ts
@@ -4,11 +4,12 @@ import { Workplace } from './about.workplace';
 import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { ProfessionalSkill } from './about.professionalskill';
+import { College } from './about.college';
 @Injectable()
 export class AboutService{
     constructor(private http: Http){}
 
-
+// -----------------------------------------------------------------------------------
 // WORKPLACE START HERE
 saveWorkplace(workplace: Workplace){
     const body = JSON.stringify(workplace);
@@ -21,10 +22,8 @@ saveWorkplace(workplace: Workplace){
 
             return this.http.post('http://localhost:8080/zuul-profileAbout/workplace/add', body, options);
         }
-
-
-
 }
+  // ------------------------------
 getWorkplaceByUID(uid: string): Observable<Workplace[]>{
     return this.http.get('http://localhost:8080/zuul-profileAbout/workplace/getAll/' + uid)
     .pipe(map((response: Response) => response.json(),
@@ -33,7 +32,7 @@ getWorkplaceByUID(uid: string): Observable<Workplace[]>{
 
 
 }
-
+// ---------------------------------
 getWorkplaceById(id: string): Observable<Workplace>{
     return this.http.get('http://localhost:8080/zuul-profileAbout/workplace/single/' + id)
     .pipe(map((response: Response) => response.json(),
@@ -42,7 +41,7 @@ getWorkplaceById(id: string): Observable<Workplace>{
 
 
 }
-
+// --------------------------------
 
 deleteWorkplaceById(id: string) {
     return this.http.delete('http://localhost:8080/zuul-profileAbout/workplace/single/delete/' + id);
@@ -50,7 +49,7 @@ deleteWorkplaceById(id: string) {
 }
 
 
-
+// ------------------------------------------------------------------------------------------
 
 // PROFESSIONAL SKILLS START HERE
 saveProfessionalSkills(professionalSkill: ProfessionalSkill){
@@ -69,6 +68,7 @@ saveProfessionalSkills(professionalSkill: ProfessionalSkill){
 
 
 }
+// -----------------------------
 getProfessionalSkillsByUID(uid: string): Observable<ProfessionalSkill[]>{
     return this.http.get('http://localhost:8080/zuul-profileAbout/professionalSkills/getAll/' + uid)
     .pipe(map((response: Response) => response.json(),
@@ -77,7 +77,7 @@ getProfessionalSkillsByUID(uid: string): Observable<ProfessionalSkill[]>{
 
 
 }
-
+// ------------------------------
 getProfessionalSkillsById(id: string): Observable<ProfessionalSkill>{
     return this.http.get('http://localhost:8080/zuul-profileAbout/professionalSkills/single/' + id)
     .pipe(map((response: Response) => response.json(),
@@ -87,7 +87,7 @@ getProfessionalSkillsById(id: string): Observable<ProfessionalSkill>{
 
 }
 
-
+// ---------------------------------
 deleteProfessionalSkillsById(id: string) {
     return this.http.delete('http://localhost:8080/zuul-profileAbout/professionalSkills/single/delete/' + id);
 
@@ -95,6 +95,52 @@ deleteProfessionalSkillsById(id: string) {
 
 
 
+// ------------------------------------------------------------------------------------------
+
+// COLLEGE SKILLS START HERE
+saveCollege(college: College){
+    const body = JSON.stringify(college);
+        const headers = new Headers({ 'Content-Type': 'application/json' });
+        const options = new RequestOptions({ headers: headers });
+        if (college.id) {
+            return this.http.put('http://localhost:8080/zuul-profileAbout/college/update/'
+             + college.id, body, options);
+
+        } else {
+
+            return this.http.post('http://localhost:8080/zuul-profileAbout/college/add', body, options);
+        }
+
+
+
+}
+// -----------------------------
+getCollegeByUID(uid: string): Observable<College[]>{
+    return this.http.get('http://localhost:8080/zuul-profileAbout/college/getAll/' + uid)
+    .pipe(map((response: Response) => response.json(),
+    catchError(this.handlError)));
+
+
+
+}
+// ------------------------------
+getCollegeById(id: string): Observable<College>{
+    return this.http.get('http://localhost:8080/zuul-profileAbout/college/single/' + id)
+    .pipe(map((response: Response) => response.json(),
+    catchError(this.handlError)));
+
+
+
+}
+
+// ---------------------------------
+deleteCollegeById(id: string) {
+    return this.http.delete('http://localhost:8080/zuul-profileAbout/college/single/delete/' + id);
+
+}
+
+
+// ----------------------------------------------------------------------------------------
 // ERROR HANDLER
 public handlError(error: Response) {
     return Observable.throw(error);
